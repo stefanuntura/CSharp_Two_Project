8bf945a2f0885ea39550190367d27c1fa5e69295
stefanuntura <stefan.untura@student.nhlstenden.com>
2022-07-02 11:07:17 +0200

    Boss Level One timer and attack patterns
    Implement BossLevelOneController
    Add attackPattern methods to BossLevelOne
    Add timer to BossLevelOne

---
+++ Graduation/BossLevelOneController.cs
@@ -0,0 +1,23 @@
+﻿using Graduation.Entities;
+using Graduation.TestMap;
+
+namespace Graduation
+{
+    class BossLevelOneController
+    {
+        private BossLevelOne _bossLevelOne;
+
+        public BossLevelOneController(BossLevelOne bossLevelOne)
+        {
+            _bossLevelOne = bossLevelOne;
+        }
+
+        public void handleAttackPatterns(float timer, Map map)
+        {
+            if(timer % 3 == 0)
+            {
+                _bossLevelOne.dashAttack(map);
+            }
+        }
+    }
+}
--- Graduation/Entities/BossLevelOne.cs
+++ Graduation/Entities/BossLevelOne.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Graduation.Graphics;
 using Graduation.Animations;
+using Graduation.TestMap;
 
 namespace Graduation.Entities
 {
@@ -19,25 +21,33 @@ namespace Graduation.Entities
         public double Damage { get; set; }
         public int DrawOder { get; set; }
 
+        float Time = 0;
+        BossLevelOneController controller;
+        String _direction = "right";
+        SpriteFont font = null;
+
         public BossLevelOne(Game game, Vector2 position) : base(game, position)
         {
             LoadContent(game);
+            controller = new BossLevelOneController(this);
         }
          
-
-
-        public void Update(GameTime gameTime)
+        public void Update(GameTime gameTime, Map map)
         {
-
+            Time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _animationSprite.Update(gameTime);
+            controller.handleAttackPatterns(Time, map);
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             _animationSprite.Draw(spriteBatch, Position);
+            //spriteBatch.DrawString(font, "The timer is:" + Time.ToString("0.00"), new Vector2(300, 50), Color.Black);
         }
 
         public void LoadContent(Game game)
         {
+            //font = ContentManager.Load<SpriteFont>("Fonts/Font.spritefont");
             _animationSprite = new AnimationSprite(new Dictionary<string, Animation>()
             {
               { "WalkRight", new Animation(game.Content.Load<Texture2D>("Player/WalkRight"), 2) },
@@ -58,6 +68,85 @@ namespace Graduation.Entities
         {
             Animation currAnim = _animationSprite.CurrentAnimation;
             Dimensions = new Vector2(currAnim.FrameWidth, currAnim.FrameHeight);
+        }
+
+        public void dashRight(Map map)
+        {
+            _animationSprite.SetActive("WalkRight");
+            _direction = "right";
+            changePlayerDimensions();
+            if (Position.X < 1265)
+            {
+                bool collision = false;
+                Box collidedBox = null;
+                Vector2 newPos = new Vector2(Position.X + (float)Speed * Time, Position.Y);
+
+                foreach (Box box in map.Boxes)
+                {
+                    if (collided(box, newPos))
+                    {
+                        collision = true;
+                        collidedBox = box;
+                        break;
+                    }
+                }
+
+                if (!collision)
+                {
+                    Position = newPos;
+                }
+                else
+                {
+                    Position = new Vector2(collidedBox.Position.X - Dimensions.X - 1, Position.Y);
+                }
+            }
+        }
+
+        public void dashLeft(Map map)
+        {
+            _animationSprite.SetActive("WalkLeft");
+            _direction = "left";
+            changePlayerDimensions();
+            if (Position.X > 0)
+            {
+                bool collision = false;
+                Box collidedBox = null;
+                Vector2 newPos = new Vector2(Position.X - (float)Speed * Time, Position.Y);
+
+                foreach (Box box in map.Boxes)
+                {
+                    if (collided(box, newPos))
+                    {
+                        collision = true;
+                        collidedBox = box;
+                        break;
+                    }
+                }
+
+                if (!collision)
+                {
+                    Position = newPos;
+                }
+                else
+                {
+                    Position = new Vector2(collidedBox.Position.X + collidedBox.Dimensions.X + 1, Position.Y);
+                }
+            }
+        }
+
+        public void dashAttack(Map map) 
+        {
+            if(_direction == "right")
+            {
+                dashLeft(map);
+                dashRight(map);
+            }
+
+            if(_direction == "left")
+            {
+                dashRight(map);
+                dashLeft(map);
+            }
         }
     }
 }
