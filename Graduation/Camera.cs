d28ce655b1643b5daa3a09a36e0c217a6f563470
SuperChalupa97 <69051047+SuperChalupa97@users.noreply.github.com>
2022-07-03 16:44:56 +0200

    Camera map (#2)

---
+++ Graduation/Camera.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Graduation.Entities;
+using Graduation.States;
+
+namespace Graduation
+{
+    class Camera
+    {
+        public Matrix Transform { get; private set; }
+        public Matrix HpTransform { get; private set; }
+
+        public void Follow(Player target) 
+        {
+            var position = Matrix.CreateTranslation(
+                -target.Position.X,
+                -target.Position.Y,
+                0);
+
+            var offset = Matrix.CreateTranslation(
+                Game1.ScreenWidth / 2,
+                Game1.ScreenHeight / 2,
+                0);
+            Transform = position * offset;
+        }
+
+        public void snapHpBar(Healthbar bar, Player target) 
+        {
+            var position = Matrix.CreateTranslation(
+               -target.Position.X,
+               -target.Position.Y,
+               0);
+
+            var offset = Matrix.CreateTranslation(
+                Game1.ScreenWidth / 2,
+                Game1.ScreenHeight / 2,
+                0);
+            Transform = position * offset;
+        }
+    }
+}
