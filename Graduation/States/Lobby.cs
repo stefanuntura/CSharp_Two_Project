b2cc7248ad4b8ed2772998be4e813dceb465996a
lineeam <72556262+lineeam@users.noreply.github.com>
2022-07-08 03:45:03 +0200

    Finish Lobby functionality (#15)
    * Add Lobby Dialogue + Player speed animation
    
    * Add transition screen

--- Graduation/States/Lobby.cs
+++ Graduation/States/Lobby.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Graduation.Animations;
 
 using Graduation.Entities;
 
@@ -23,14 +24,21 @@ namespace Graduation.States
         private Texture2D _weaponControls;
         private Texture2D _generalControls;
         private Texture2D _goodLuck;
-        private bool _control1 = true;
-        private List<String> _conversation;
+        private Texture2D _plant;
+        private Texture2D _couch;
+        private Texture2D _year;
+
+        private AnimationSprite _animationSprite;
+
+        private bool moveState =  false;
+        private double _displayTimer;
+        private double _transitionTimer;
+
         public Lobby(Game1 game, GraphicsDevice graphicsDevice, ContentManager contentManager) : base(game, graphicsDevice, contentManager)
         {
             //Initialize map
             _map = new TestMap.Map();
             _player = new Player(game, new Vector2(50,50));
-            _conversation = new List<String>();
             LoadContent(game);
 
             for (int i = 0; i < 30; i++)
@@ -46,8 +54,6 @@ namespace Graduation.States
                 // black floor blocks
                 _map.addBox(new TestMap.Box(game, new Vector2(32 * 18, 150), new Vector2(0, 330), Color.Black));
                 _map.addBox(new TestMap.Box(game, new Vector2(100, 150), new Vector2(32 * 21, 330), Color.Black));
-
-                _conversation.Add("");
             }
         }
 
@@ -55,44 +61,70 @@ namespace Graduation.States
         {
 
             _spriteBatch.Begin();
-
-            for (int i = 0; i < 30; i++)
+            if (!moveState)
             {
-                for (int j = 0; j < 20; j++)
+                for (int i = 0; i < 30; i++)
                 {
-                    //Draw Backgroundpsprite
-                    _spriteBatch.Draw(_wall, new Vector2(i * 32, j * 32), Color.White);
+                    for (int j = 0; j < 20; j++)
+                    {
+                        //Draw Backgroundpsprite
+                        _spriteBatch.Draw(_wall, new Vector2(i * 32, j * 32), Color.White);
+                    }
                 }
+                _map.Draw(_spriteBatch, gameTime);
+
+                //Draw design elements
+                _spriteBatch.Draw(_reception, new Vector2(120, 287), Color.White);
+                _spriteBatch.Draw(_drop, new Vector2(18 * 32, 335), Color.White);
+                _spriteBatch.Draw(_halfFloor, new Vector2(18 * 32, 330), Color.White);
+                _spriteBatch.Draw(_couch, new Vector2(330, 330 - _couch.Bounds.Height), Color.White);
+                _spriteBatch.Draw(_couch, new Vector2(390, 330 - _couch.Bounds.Height), Color.White);
+
+                _player.Draw(_spriteBatch, gameTime, this, _map.Enemies.Count);
+
+                for (int i = 0; i < 30; i++)
+                {
+                    if ((i != 0 && i < 18) || (i > 20 && i < 24))
+                    {
+                        // Draw floor sprites
+                        _spriteBatch.Draw(_floor, new Vector2(i * 32, 330), Color.White);
+                    }
+                    for (int j = 0; j < 20; j++)
+                    {
+                        //Draw Black blocks in hole
+                        if (i > 17 && i < 21 && j > 13)
+                            _spriteBatch.Draw(_blackBlock, new Vector2(i * 32, j * 32), Color.White);
+                    }
+                }
+
+                //Draw Dialogue bubble
+                if (_displayTimer > 5 && _player.Position.X > 120 && _player.Position.X < 200)
+                {
+                    int sprite = (int)Util.RandomDouble(0, 3);
+                    _animationSprite.SetActive(sprite.ToString());
+                    _displayTimer = 0;
+                }
+                else if (_displayTimer <= 5 && _player.Position.X > 120 && _player.Position.X < 200)
+                {
+                    _animationSprite.Draw(_spriteBatch, new Vector2(80, 80));
+                }
+
+
+                // display control explanation
+                _spriteBatch.Draw(_player.Position.Y > 320 || _player.Position.X > 600 ? _goodLuck : _player.Position.X > 300 ? _weaponControls : _generalControls, new Vector2(430, 100), Color.Black * 0.5f);
             }
-            _map.Draw(_spriteBatch, gameTime);
-
-            //Draw design elements
-            _spriteBatch.Draw(_reception, new Vector2(120, 287), Color.White);
-            _spriteBatch.Draw(_drop, new Vector2(18 * 32, 335), Color.White);
-            _spriteBatch.Draw(_halfFloor, new Vector2(18 * 32, 330), Color.White);
-
-            _player.Draw(_spriteBatch, gameTime, this, _map.Enemies.Count);
-
-            for (int i = 0; i < 30; i++)
+            else
             {
-                if (( i != 0 && i < 18) || ( i > 20 && i < 24))
-                {
-                    // Draw floor sprites
-                    _spriteBatch.Draw(_floor, new Vector2(i * 32, 330), Color.White);
-                }
-                for (int j = 0; j < 20; j++)
-                {
-                    //Draw Black blocks in hole
-                    if(i > 17 && i < 21 && j > 13)
-                        _spriteBatch.Draw(_blackBlock, new Vector2(i * 32, j *32), Color.White);
-                }
+                for (int i = 0; i < 30; i++)
+                    for (int j = 0; j < 20; j++)
+                            _spriteBatch.Draw(_blackBlock, new Vector2(i * 32, j * 32), Color.Black);
+
+                _spriteBatch.Draw(_year, new Vector2(280, 230), Color.White);
             }
 
-            // display control explanation
-            _spriteBatch.Draw(_player.Position.Y > 320 ? _goodLuck : _player.Position.X > 300 ?_weaponControls : _generalControls, new Vector2(430, 100), Color.Black * 0.5f);
-
-            _spriteBatch.End();
+                _spriteBatch.End();
         }
+
 
         public void LoadContent(Game game)
         {
@@ -105,14 +137,30 @@ namespace Graduation.States
             _weaponControls = game.Content.Load<Texture2D>("Lobby/weapon");
             _generalControls = game.Content.Load<Texture2D>("Lobby/control");
             _goodLuck = game.Content.Load<Texture2D>("Lobby/gl");
+            _couch = game.Content.Load<Texture2D>("Lobby/couch");
+            _plant = game.Content.Load<Texture2D>("Lobby/plant");
+            _year = game.Content.Load<Texture2D>("Lobby/Y1");
+
+            _animationSprite = new AnimationSprite(new Dictionary<string, Animation>()
+            {
+              { "0", new Animation(game.Content.Load<Texture2D>("Lobby/conv0"), 1) },
+              { "1", new Animation(game.Content.Load<Texture2D>("Lobby/conv1"), 1) },
+              { "2", new Animation(game.Content.Load<Texture2D>("Lobby/conv2"), 1) },
+            }, "0", Color.White * 0.75f);
         }
 
         public override void Update(GameTime gameTime)
         {
+            //update conversation timer
+            _displayTimer += gameTime.ElapsedGameTime.TotalSeconds;   
+
             //Move to next gamestate
             if (_player.Position.Y > 430)
             {
-                _game.ChangeState(new GameState(_game, _graphicsDevice, _contentManager));
+                _transitionTimer += gameTime.ElapsedGameTime.TotalSeconds;
+                moveState = true;
+                if(_transitionTimer > 2)
+                    _game.ChangeState(new GameState(_game, _graphicsDevice, _contentManager));
             }
 
             // Place player back at spawn if they move outside map
@@ -121,8 +169,7 @@ namespace Graduation.States
                 _player.Position = new Vector2(50, 250);
             }
 
-            //set bool for switch between instructions
-            _control1 = _player.Position.X < 250 ? true : false;
+            _animationSprite.Update(gameTime);
 
             _player.Update(gameTime, _map, this);
             _player.weapon.Update(gameTime, _player, _map.Enemies, _map);
